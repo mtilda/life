9dbbdc3171a33bff119dda428d7f15e14c760c65
Kay Vail <codingwithkay@gmail.com>
2020-11-07 17:34:21 -0500

    Bootstrapped with create-react-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./Components/Header/Header";
+import { GameboardContainer as Gameboard } from "./Components/Gameboard/Gameboard.container";
+import "./App.css";
+
+export default () => {
+  return (
+    <div className="App">
+      <Header />
+      <Gameboard />
+    </div>
+  );
+}
