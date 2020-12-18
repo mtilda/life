e98eca52b32c3adb901b8abcc9e719b7d70c2cb1
mxkay <codingwithkay@gmail.com>
2020-12-17 23:47:38 -0500

    Set up router

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
-import { useState } from 'react';
-import Header from './Components/Header/Header';
-import TickCounter from './Components/TickCounter/TickCounter';
-import { GameboardContainer as Gameboard } from './Components/Gameboard/Gameboard.container';
+import { Link, Redirect, Route, Switch } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import GamePage from './Pages/GamePage';
 import './App.css';
 
-export default () => {
-  const [tick, setTick] = useState(0);
-  const [play, setPlay] = useState(true);
-  const [reset, setReset] = useState(true);
+export default () =>
+  <div>
+    {/* <ul>
+      <li>
+        <Link to='/'>Home</Link>
+      </li>
+      <li>
+        <Link to='/game'>Game</Link>
+      </li>
+    </ul> */}
 
-  const handlePlay = () => {
-    setPlay(true);
-  };
-
-  const handlePause = () => {
-    setPlay(false);
-  };
-
-  const handleReset = () => {
-    setReset(true);
-  };
-
-  const dismissReset = () => {
-    setReset(false);
-  };
-
-  return (
-    <div className='app'>
-      <Header play={play} handlePlay={handlePlay} handlePause={handlePause} handleReset={handleReset} />
-      <TickCounter tick={tick} />
-      <Gameboard matrixSize={64} play={play} reset={reset} dismissReset={dismissReset} tick={tick} setTick={setTick} />
-    </div>
-  );
-};
+    <Switch>
+      <Route exact path='/'>
+        <HomePage />
+      </Route>
+      <Route path='/game'>
+        <GamePage />
+      </Route>
+      <Redirect path='*' to='/' />
+    </Switch>
+  </div>;
---
+++ src/Pages/GamePage.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import Header from '../Components/Header/Header';
+import TickCounter from '../Components/TickCounter/TickCounter';
+import { GameboardContainer as Gameboard } from '../Components/Gameboard/Gameboard.container';
+
+export default () => {
+  const [tick, setTick] = useState(0);
+  const [play, setPlay] = useState(true);
+  const [reset, setReset] = useState(true);
+
+  const handlePlay = () => {
+    setPlay(true);
+  };
+
+  const handlePause = () => {
+    setPlay(false);
+  };
+
+  const handleReset = () => {
+    setReset(true);
+  };
+
+  const dismissReset = () => {
+    setReset(false);
+  };
+
+  return (
+    <>
+      <Header play={play} handlePlay={handlePlay} handlePause={handlePause} handleReset={handleReset} />
+      <TickCounter tick={tick} />
+      <Gameboard matrixSize={64} play={play} reset={reset} dismissReset={dismissReset} tick={tick} setTick={setTick} />
+    </>
+  );
+};
