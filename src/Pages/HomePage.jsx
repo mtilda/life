b4a38277f2ee1e382f962692d16599f111b75e8a
mxkay <codingwithkay@gmail.com>
2021-01-19 20:32:51 -0500

    Build home page

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,3 +1,20 @@
+import ButtonLink from '../Components/ButtonLink/ButtonLink';
+import styled from 'styled-components';
+
+const HomePage = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
 export default () => {
-  return <h1>Home</h1>;
+  return (
+    <HomePage>
+      <h1>Conway's Game of Life</h1>
+      <br aria-hidden='true' />
+      <ButtonLink to='/game'>Open Game</ButtonLink>
+    </HomePage>
+  );
 };
