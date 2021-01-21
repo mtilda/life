f05053378aef0045f3d313e47de7b6b9ca09b338
mxkay <codingwithkay@gmail.com>
2021-01-21 18:40:27 -0500

    Remove cell

--- src/Components/Cell/Cell.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Cell.css';
-
-export default ({ row, column, isAlive }) =>
-  <div
-    className='cell' id={`cell-${row}-${column}`} style={{
-      backgroundColor: typeof isAlive === 'undefined' ? 'red' : isAlive ? 'lightgray' : 'lightslategray'
-    }}
-  />;
